e15655046e02b326aa122f0a777422ceb59668e6
Markus Winkler <winkler.markus@me.com>
2021-05-10 15:31:06 +0200

    calc viewing conditions

--- collectors-enabled/astroweather.py
+++ collectors-enabled/astroweather.py
@@ -5,6 +5,9 @@ information from 7Timer by it's RESTful API, calculates some
 metrics and creates a dictionary which is feedede into Prometheus
 by the api-collector.
 
+The metrics calculated reflect the expected astro view conditions of
+this night based on cloud coverage, seeing and atmospheric transparency.
+
 This file has one single funtion collect(), which is called by the
 api-collector. For testing purposes, one can directly run the collector
 with python3 <FILENAME>. You need to ensure the presence of the
@@ -18,6 +21,9 @@ from datetime import datetime, timedelta
 import logging
 
 _LOGGER = logging.getLogger(__name__)
+
+LONGITUDE=11.985
+LATITUDE=48.313
 
 def collect() -> dict:
     """
@@ -47,13 +53,13 @@ def collect() -> dict:
     # Define your metrics here
     result = {
         "CounterMetricFamilyName": "astroweather",
-        "CounterMetricFamilyHelpText": "AstroWeather Cloudcoverage",
+        "CounterMetricFamilyHelpText": "AstroWeather this Nights Cloudcoverage",
         "CounterMetricFamilyLabels": ['init', 'timepoint'],
         "Metrics": []
     }
 
     # API query and response parsing here
-    url = "http://www.7timer.info/bin/api.pl?lon=11.985&lat=48.313&product=astro&output=json"
+    url = "http://www.7timer.info/bin/api.pl?lon=" + str(LONGITUDE) + "&lat=" + str(LATITUDE) + "&product=astro&output=json"
     data = {}
     post_header = {
         "Content-type": "application/json",
@@ -69,17 +75,41 @@ def collect() -> dict:
         if response['message'] == "Invalid API Key":
             raise ValueError("Invalid API Key")
 
-    init = datetime.strptime(response['init'], "%Y%m%d%H")
+    # Timezone Offset is currently calculated based on system time
+    offset = time.timezone if (time.localtime().tm_isdst == 0) else time.altzone
+    offset = offset / 60 / 60 * (-1)
 
+    _LOGGER.debug("Timezone offset: " + str(offset))
+
+    init = datetime.strptime(response['init'], "%Y%m%d%H") + timedelta(hours=offset)
+    
     # Calculate your metrics
     if len(response['dataseries']) > 0:
         for forecast in response['dataseries']:
+            forecast_time = init + timedelta(hours=forecast['timepoint'])
+
+            tomorrow_3am = (datetime.now() + timedelta(days=1)).replace(hour=3, minute=0, second=0, microsecond=0)
+            if  forecast_time > tomorrow_3am:
+                continue
+
+            hour_of_day = forecast_time.hour % 24
+            if hour_of_day > 3 and hour_of_day < 19:
+                continue
+
+            cloudcover = forecast["cloudcover"]
+            seeing = forecast["seeing"]
+            transparency = forecast["transparency"]
+
+            # Calculate Condition
+            # round( (3*cloudcover + seeing + transparency) / (3*9+8+8) * 5)
+            condition = round(
+                (3 * cloudcover + seeing + transparency) / 43 * 5,
+            )
+            
             labels = []
             labels.append(str(init))
-            labels.append(str(init + timedelta(hours=forecast['timepoint'])))
-            metric = forecast['cloudcover']
-            if not isinstance(metric, int):
-                metric = 0
+            labels.append(str(forecast_time))
+            metric = condition
 
             # Add a single metric
             result['Metrics'].append([labels, metric])
